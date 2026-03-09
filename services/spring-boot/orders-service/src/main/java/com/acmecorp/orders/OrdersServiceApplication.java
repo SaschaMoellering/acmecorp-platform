2ac866cd53e60fc3e0477f484f7b0c2f00db0da4
Sascha Moellering <sascha.moellering@gmail.com>
2026-03-09 23:41:26 +0100

    Add orders startup instrumentation

--- services/spring-boot/orders-service/src/main/java/com/acmecorp/orders/OrdersServiceApplication.java
+++ services/spring-boot/orders-service/src/main/java/com/acmecorp/orders/OrdersServiceApplication.java
@@ -1,11 +1,13 @@
 package com.acmecorp.orders;
 
+import com.acmecorp.orders.startup.StartupTimeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrdersServiceApplication {
     public static void main(String[] args) {
+        StartupTimeline.markJvmMainStart();
         SpringApplication.run(OrdersServiceApplication.class, args);
     }
 }
