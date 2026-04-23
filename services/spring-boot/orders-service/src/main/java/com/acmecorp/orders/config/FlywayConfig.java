b2700c4b38b1e59deabbe919912f4af020af001c
Sascha Moellering <sascha.moellering@gmail.com>
2026-04-23 21:15:13 +0200

    Fix java25 local benchmark startup blockers

---
+++ services/spring-boot/orders-service/src/main/java/com/acmecorp/orders/config/FlywayConfig.java
@@ -0,0 +1,33 @@
+package com.acmecorp.orders.config;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationVersion;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.jpa.autoconfigure.EntityManagerFactoryDependsOnPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class FlywayConfig {
+
+    @Bean(initMethod = "migrate")
+    @ConditionalOnMissingBean(Flyway.class)
+    public Flyway flyway(DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .baselineOnMigrate(true)
+                .baselineVersion(MigrationVersion.fromVersion("0"))
+                .locations("classpath:db/migration")
+                .load();
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    static class EntityManagerFactoryDependsOnFlywayPostProcessor extends EntityManagerFactoryDependsOnPostProcessor {
+
+        EntityManagerFactoryDependsOnFlywayPostProcessor() {
+            super("flyway");
+        }
+    }
+}
